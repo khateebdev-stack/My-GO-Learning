3ad8235c41005aa9023cbdd5c6ac5602edd16d10
Khateeb Ur Rehman <skrmc2013@gmail.com>
2025-09-20 00:03:47 +0500

    update_with_basic_learning

---
+++ fmt-package-Indepth.go
@@ -0,0 +1,11 @@
+package main
+
+// What is fmt package?
+/*
+It is most important packagen in Go that is one of the most widely used packages in Go
+
+*/
+func FmtPackage(){
+	var item1, item2 int 
+
+}
